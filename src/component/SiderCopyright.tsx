b92e0b5d845ede73be573f0c69690d188201869f
Sidia <sidiastudios@gmail.com>
2018-01-26 01:18:23 +0100

    Cleanup

--- src/component/SiderCopyright.tsx
+++ src/component/SiderCopyright.tsx
@@ -8,6 +8,7 @@ class SiderCopyright extends React.Component<TSiderCopyrightProps, TSiderCopyrig
     render() {
         return <div className="copyright">
             Developed by <a href="https://github.com/SidiaDevelopment/" target="_blank">Sidia </a><br />
+            <a href="https://github.com/SidiaDevelopment/GarlicoinWallet" target="_blank"><Icon type="github" /></a>&nbsp;|&nbsp;
             <a href="https://gitlab.sidia.net/Sidia/GarliWallet" target="_blank"><Icon type="gitlab" /></a>
         </div>
     }
